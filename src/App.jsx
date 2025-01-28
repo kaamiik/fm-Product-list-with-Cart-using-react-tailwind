31c77ce8cac98c954efc80499513111c21056df7
Kamran Kiani <kiani.km@gmail.com>
2025-01-28 16:33:47 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Footer from './components/Footer';
+import Main from './components/Main';
+
 function App() {
-  return <h1 className="text-amber-400">Hello react and tailwind</h1>;
+  return (
+    <div className="text-300 flex min-h-screen flex-col bg-rose-50 font-serif font-normal">
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
